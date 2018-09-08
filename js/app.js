fe0fc93a1ec64d4cd341f6e448d22f9624afc927
ribo <roibeard.mac.unfraidh@gmail.com>
2018-09-08 16:35:34 +0200

    add and remove typing text div on pig1 mouseover mouseout

--- js/app.js
+++ js/app.js
@@ -10,158 +10,119 @@ var audio = new Audio("./audio/scottHappy.mp3");
 var rShot = new Audio("./audio/RifleShot.wav");
 var squeal1 = new Audio("./audio/PigSqueal.wav");
 
-window.onload=function(){    audio.play();};
-var rifle = new Audio("./audio/Rifle.wav");
 let weaponOn = false;
-pig1.onclick = function () {
-    if (weaponOn) {
-        console.log("bang!");
-        audio.pause();
-        rShot.play();
-        squeal1.play();
-        //flash
-   
-        pig2.style.display="none";
-        pig3.style.display="none";
-        pig4.style.display="none";
+var rifle = new Audio("./audio/Rifle.wav");
+$(document).ready(function () {
 
-        window.setTimeout(endShow(), 2000);
+    // pigs.classList.remove("hidden");
 
-    } else {
-    document.getElementsByClassName("locker")[0].classList.remove("hidden");
-    document.getElementsByClassName("pigInfo")[0].classList.remove("hidden");
-    document.getElementsByClassName("pigInfo")[0].innerHTML = "";
-    document.getElementsByClassName("locker")[0].classList.add("fade-in");
-    document.getElementsByClassName("pigInfo")[0].innerHTML = `Age: 7 months
-   Weight:90kg
-   Gender: female 
-   Health: healthy`;
+    // document.getElementsByTagName("body")[0].classList.remove("step0");
+    // document.getElementsByTagName("body")[0].classList.add("step1");
+
+    // document.getElementsByTagName("nav")[0].classList.add("hidden");
+
+    // document.getElementsByClassName("logo")[0].classList.add("hidden");
+    /*
+    //////////////////////////////////////////////////////////
+    TEMPORARY FORCE APP TO START ON TUTORIAL STEP1
+    ////////////////////////////////////////////////////////
+    */
+    $('.pigs').removeClass('hidden');
+    $('#appFrame').removeClass('step0');
+    $('#appFrame').addClass('step1');
+    $('nav').addClass('hidden');
+    $('.logo').addClass('hidden');
+    //////////////////////////////////////////////////
+
+    $('#pig1').on('mouseenter', function () {
+        $('.pigInfo').removeClass('hidden');
+        $( ".pigInfo" ).append('<p id="t"></p>'  );
+        console.log("t added");
+        showPigStats();
+    });
+
+    $('#pig1').on('mouseout', function () {
+      $('.pigInfo').addClass('hidden');
+  
+    $('#t').remove();
+    console.log("t removed!");
+ 
+    });
+
+
+
+    function showPigStats() {
+
+        // document.getElementsByClassName("pigInfo")[0].classList.remove("hidden");
+        // document.getElementsByClassName("pigInfo")[0].innerHTML = "";
+        // document.getElementsByClassName("locker")[0].classList.add("fade-in");
+
+        $('#t').t(`
+       Age: 7 months<br/>
+       Weight:90kg <br/>
+       Gender: female<br/>
+       Health: healthy`);
     }
 
-    // squeal.play();
-
-};
-
-
-pig2.onclick = function () {
-    if (weaponOn) {
-        console.log("bang!");
-        console.log("bang!");
-        audio.pause();
-        rShot.play();
-        squeal1.play();
-        //flash
-   
-        pig1.style.display="none";
-        pig3.style.display="none";
-        pig4.style.display="none";
-
-    } else {
-    document.getElementsByClassName("locker")[0].classList.remove("hidden");
-    document.getElementsByClassName("locker")[0].classList.add("fade-in");
-    document.getElementsByClassName("pigInfo")[0].classList.remove("hidden");
-    document.getElementsByClassName("pigInfo")[0].innerHTML = "";
-    document.getElementsByClassName("pigInfo")[0].innerHTML =
-        `Age: 11 months
-    Weight:80kg
-    Gender: Male 
-    Health: healthy`;}
-};
-
-pig3.onclick = function () {
-
-    if (weaponOn) {
-        console.log("bang!");
-        audio.pause();
-        rShot.play();
-        squeal1.play();
-        //flash
-   
-        pig2.style.display="none";
-        pig1.style.display="none";
-        pig4.style.display="none";
-        console.log("bang!");
-    
-    } else {
-    document.getElementsByClassName("locker")[0].classList.remove("hidden");
-    document.getElementsByClassName("locker")[0].classList.add("fade-in");
-    document.getElementsByClassName("pigInfo")[0].classList.remove("hidden");
-    document.getElementsByClassName("pigInfo")[0].innerHTML = "";
-    document.getElementsByClassName("pigInfo")[0].innerHTML =
-        `Age: 10 months
-    Weight:84kg
-    Gender: Male 
-    Health: healthy`;}
-};
-
-
-pig4.onclick = function () {
-    if (weaponOn) {
-        console.log("bang!");
-        audio.pause();
-        rShot.play();
-        squeal1.play();
-        //flash
-   
-        pig2.style.display="none";
-        pig3.style.display="none";
-        pig1.style.display="none";
-        console.log("bang!");
-
-    } else {
-        document.getElementsByClassName("locker")[0].classList.remove("hidden");
-        document.getElementsByClassName("locker")[0].classList.add("fade-in");
-
-        document.getElementsByClassName("pigInfo")[0].classList.remove("hidden");
-        document.getElementsByClassName("pigInfo")[0].innerHTML = "";
-
-        document.getElementsByClassName("pigInfo")[0].innerHTML =
-            `Age: 9 months
-    Weight:120kg
-    Gender: female 
-    Health: healthy`;
-    }
-};
-document.getElementById('tutorial').addEventListener("click", function () {
-    pigs.classList.remove("hidden");
-
-    document.getElementsByTagName("body")[0].classList.remove("step0");
-    document.getElementsByTagName("body")[0].classList.add("step1");
-
-    document.getElementsByTagName("nav")[0].classList.add("hidden");
-
-    document.getElementsByClassName("logo")[0].classList.add("hidden");
 
 });
 
 
-locker.onclick = function () {
-    console.log("locker clicked");
-    locker.style.border = "2px solid orange";
-    document.getElementsByClassName("tools")[0].classList.remove("hidden");
-    document.getElementsByClassName("tools")[0].classList.add("fade-in");
-    document.getElementsByClassName("tools")[0].innerHTML = `Rifle<br />
-    <br />
-    Stun Gun
-    <br /><br />
-    Bolt Gun`;
-    
-};
+// pig1.onclick = function () {
+//     if (weaponOn) {
+//         console.log("bang!");
+//         audio.pause();
+//         rShot.play();
+//         squeal1.play();
+//         //flash
 
-tools.onclick = function () {
+//         pig2.style.display="none";
+//         pig3.style.display="none";
+//         pig4.style.display="none";
 
-    document.getElementsByClassName("locker")[0].classList.add("hide");
+//         window.setTimeout(endShow(), 2000);
 
-    document.getElementsByClassName("pigInfo")[0].classList.add("hide");
-    weaponOn = true;
-    rifle.play();
-}
+//     } else {
+//     document.getElementsByClassName("locker")[0].classList.remove("hidden");
 
-function endShow(){
 
-    document.getElementsByTagName("body")[0].classList.remove("step1");
-    document.getElementsByTagName("body")[0].classList.add("step2");
-    document.getElementsByClassName("tools")[0].classList.add("hidden");
-    document.getElementsByClassName("pig1")[0].classList.add("hidden");
-    
-}
+//     // squeal.play();
+
+// };
+// }
+
+
+// document.getElementById('tutorial').addEventListener("click", function () {
+//     pigs.classList.remove("hidden");
+
+//     document.getElementsByTagName("body")[0].classList.remove("step0");
+//     document.getElementsByTagName("body")[0].classList.add("step1");
+
+//     document.getElementsByTagName("nav")[0].classList.add("hidden");
+
+//     document.getElementsByClassName("logo")[0].classList.add("hidden");
+
+// });
+
+
+// locker.onclick = function () {
+//     console.log("locker clicked");
+//     locker.style.border = "2px solid orange";
+//     document.getElementsByClassName("tools")[0].classList.remove("hidden");
+//     document.getElementsByClassName("tools")[0].classList.add("fade-in");
+//     document.getElementsByClassName("tools")[0].innerHTML = `Rifle<br />
+//     <br />
+//     Stun Gun
+//     <br /><br />
+//     Bolt Gun`;
+
+// };
+
+// tools.onclick = function () {
+
+//     document.getElementsByClassName("locker")[0].classList.add("hide");
+
+//     document.getElementsByClassName("pigInfo")[0].classList.add("hide");
+//     weaponOn = true;
+//     rifle.play();
+// }
